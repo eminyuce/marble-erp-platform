8e09dd733a1180bdf16b85c9e4a0502dc0743bdf
EMIN YUCE <eminyuce@gmail.com>
2026-09-21 10:13:35 +0300

    fix(site): restore StockReservation import in ConstructionSiteService

--- src/main/java/com/ozerler/marble/service/ConstructionSiteService.java
+++ src/main/java/com/ozerler/marble/service/ConstructionSiteService.java
@@ -10,6 +10,7 @@ import com.ozerler.marble.model.ProjectLocation;
 import com.ozerler.marble.model.SiteInstallation;
 import com.ozerler.marble.model.SiteSupplyAllocation;
 import com.ozerler.marble.model.Slab;
+import com.ozerler.marble.model.StockReservation;
 import com.ozerler.marble.model.enums.ExpenseCategory;
 import com.ozerler.marble.model.enums.MaterialLotStatus;
 import com.ozerler.marble.model.enums.ProjectStatus;
